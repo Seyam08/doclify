1d6adc8131bbc9b12846a5f77a10e70b0533305d
Seyam <seyam.hossain.118833@gmail.com>
2025-11-15 22:04:09 +0600

    feat (ui - dashboard) : added sidebar layout at dashboard route

---
+++ app/dashboard/account/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-4 p-4 pt-0">
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+      </div>
+      <div className="bg-muted/50" />
+    </div>
+  );
+}
