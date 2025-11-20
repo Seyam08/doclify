9d4de036beeda7549d4fe22e4b990b4d6adc4ad6
Seyam <seyam.hossain.118833@gmail.com>
2025-11-20 20:34:42 +0600

    fix (auth): bug fixed for route detect

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,14 +5,17 @@ export async function proxy(request: NextRequest) {
   const session = await auth();
   const isloggedIn = !!session?.user;
 
-  if (request.nextUrl.pathname === "/dashboard" && isloggedIn === false) {
+  if (
+    request.nextUrl.pathname.startsWith("/dashboard") &&
+    isloggedIn === false
+  ) {
     return NextResponse.redirect(new URL("/login", request.nextUrl));
   }
-  if (request.nextUrl.pathname === "/login" && isloggedIn === true) {
+  if (request.nextUrl.pathname.startsWith("/login") && isloggedIn === true) {
     return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
   }
 }
 
 export const config = {
-  matcher: ["/dashboard", "/login"],
+  matcher: ["/dashboard/:path*", "/login/:path*"],
 };
