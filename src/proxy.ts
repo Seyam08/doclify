ad8d04ecfdf6379392676361822ff1475bc84434
Seyam <seyam.hossain.118833@gmail.com>
2025-11-20 19:49:40 +0600

    feat (auth): login and dashboard route authenticated and protected

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,12 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function proxy(request: NextRequest) {
   const session = await auth();
+  const isloggedIn = !!session?.user;
 
-  if (!session?.user) {
+  if (request.nextUrl.pathname === "/dashboard" && isloggedIn === false) {
     return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+  if (request.nextUrl.pathname === "/login" && isloggedIn === true) {
+    return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
   }
 }
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/dashboard", "/login"],
 };
