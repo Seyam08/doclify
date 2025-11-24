a52bf84ee36fb017368a3eff024a039a4233a17b
Seyam <seyam.hossain.118833@gmail.com>
2025-11-24 21:30:50 +0600

    feat (editor): Added preview on add-post

--- src/app/dashboard/add-post/page.tsx
+++ src/app/dashboard/add-post/page.tsx
@@ -1,24 +1,9 @@
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
+import AddPost from "@/components/ui/add-post/add-post";
 
 export default function Page() {
   return (
     <div className="p-5">
-      <ResizablePanelGroup
-        direction="horizontal"
-        className="min-h-screen max-w-full rounded-lg border md:min-w-[450px]"
-      >
-        <ResizablePanel defaultSize={50}>
-          <div className="h-full p-3">Add post editor</div>
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={50}>
-          <div className="h-full p-3">Preview post</div>
-        </ResizablePanel>
-      </ResizablePanelGroup>
+      <AddPost />
     </div>
   );
 }
---
+++ src/components/ui/add-post/add-post.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { MinimalTiptap } from "@/components/ui/editor";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { useState } from "react";
+
+const initialContent = `
+       <h1>Welcome to Doclify ✨</h1>
+    <p>
+       Doclify is a minimal, modern blogging platform designed to help you write and publish your ideas effortlessly. 
+       This editor is powered by <strong>Tiptap</strong>, giving you a smooth and flexible writing experience with rich formatting options.
+    </p>
+    <hr/>
+    
+    <ul>
+       <li>Format your text using <strong>bold</strong>, <em>italic</em>, or <u>underline</u></li>
+       <li>Create structured content using headings and subheadings</li>
+       <li>Build ordered and unordered lists</li>
+       <li>Add blockquotes, links, code blocks, or images</li>
+       <li>Experiment with rich content to see how it renders on Doclify</li>
+    </ul>
+    
+    <blockquote>
+       <p>
+          “Good writing begins with clarity. Doclify helps you stay focused, organized, and expressive
+          — one word at a time.”
+       </p>
+    </blockquote>
+    
+      `;
+
+export default function AddPost() {
+  const [content, setContent] = useState<string>(initialContent);
+  const [html, setHtml] = useState<string>();
+
+  const handleSave = () => {
+    setHtml(content);
+  };
+
+  return (
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="min-h-screen max-w-full rounded-lg border md:min-w-[450px]"
+    >
+      <ResizablePanel defaultSize={50}>
+        <div className="h-full p-3">
+          <div className="size-full flex flex-col items-center space-y-3">
+            <div className="w-full max-w-full">
+              <MinimalTiptap
+                content={content}
+                onChange={setContent}
+                placeholder="Start typing your content here..."
+                className="min-h-[400px]"
+              />
+            </div>
+            <Button variant="outline" onClick={handleSave}>
+              Preview
+            </Button>
+          </div>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={50}>
+        <div className="h-full p-3">
+          <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+            Preview
+          </h2>
+          <div
+            dangerouslySetInnerHTML={{ __html: html || "" }}
+            className="tiptap"
+          ></div>
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+}
