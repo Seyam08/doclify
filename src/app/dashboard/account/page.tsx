54a9be0b57e2a47017d1fe10626d963d6de7dce2
Seyam <seyam.hossain.118833@gmail.com>
2025-11-24 20:25:43 +0600

    feat (editor): added tiptap editor with minimal setup

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -1,16 +1,52 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { MinimalTiptap } from "@/components/ui/editor";
+import { useState } from "react";
+
 export default function Page() {
+  const [content, setContent] = useState<string>(`
+   <h1>Welcome to Doclify ✨</h1>
+<p>
+   Doclify is a minimal, modern blogging platform designed to help you write and publish your ideas effortlessly. 
+   This editor is powered by <strong>Tiptap</strong>, giving you a smooth and flexible writing experience with rich formatting options.
+</p>
+<hr/>
+
+<ul>
+   <li>Format your text using <strong>bold</strong>, <em>italic</em>, or <u>underline</u></li>
+   <li>Create structured content using headings and subheadings</li>
+   <li>Build ordered and unordered lists</li>
+   <li>Add blockquotes, links, code blocks, or images</li>
+   <li>Experiment with rich content to see how it renders on Doclify</li>
+</ul>
+
+<blockquote>
+   <p>
+      “Good writing begins with clarity. Doclify helps you stay focused, organized, and expressive
+      — one word at a time.”
+   </p>
+</blockquote>
+
+  `);
+
   return (
-    <div className="flex flex-col gap-4 p-4 pt-0">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
+    <div className="size-full flex flex-col items-center justify-center p-4 space-y-3">
+      <div className="w-full max-w-full">
+        <MinimalTiptap
+          content={content}
+          onChange={setContent}
+          placeholder="Start typing your content here..."
+          className="min-h-[400px]"
+        />
       </div>
-      <div className="bg-muted/50" />
+      <Button
+        variant="outline"
+        onClick={() => {
+          console.log(content);
+        }}
+      >
+        Save
+      </Button>
     </div>
   );
 }
