2f12696be6d6567de640f66e301c3011b1bc4383
Seyam <seyam.hossain.118833@gmail.com>
2025-11-25 15:22:07 +0600

    feat (ui - components): added TypographyP, UnderlineLink in typography file

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -1,11 +1,13 @@
 import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { ReactNode } from "react";
 
 export function TypographyH1({
   children,
   className,
 }: {
   className?: string;
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
     <h1
@@ -16,5 +18,41 @@ export function TypographyH1({
     >
       {children}
     </h1>
+  );
+}
+
+export function TypographyP({
+  children,
+  className,
+}: {
+  className?: string;
+  children: ReactNode;
+}) {
+  return (
+    <p
+      className={cn(
+        "text-muted-foreground text-sm md:text-base leading-7 not-first:mt-6",
+        className
+      )}
+    >
+      {children}
+    </p>
+  );
+}
+
+export default function UnderlineLink({
+  href,
+  children,
+  className = "",
+}: {
+  href: string;
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <Link href={href} className={cn("group relative", className)}>
+      <span>{children}</span>
+      <span className="absolute -bottom-0.5 left-0 w-0 transition-all h-[0.2] bg-primary group-hover:w-full"></span>
+    </Link>
   );
 }
