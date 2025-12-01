1d0d0a667c9d4935f2194780b1ec94048e724368
Seyam <seyam.hossain.118833@gmail.com>
2025-12-01 20:39:29 +0600

    feat (route - categories): added categories route with single category page

---
+++ src/app/categories/[slug]/page.tsx
@@ -0,0 +1,38 @@
+import { getSingleMeta } from "@/actions/post/post-actions";
+import { DoclifyBlogCard } from "@/components/DoclifyCards/DoclifyCards";
+import { TypographyH2 } from "@/components/ui/typography";
+import { BlogType } from "@/types/schema.types";
+
+export default async function page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const decodedSlug = decodeURIComponent(slug);
+  const response = await getSingleMeta("categories", decodedSlug);
+
+  if (response.success === false) {
+    return (
+      <div className="m-auto">
+        <TypographyH2 className="mb-14">{response.message}</TypographyH2>
+      </div>
+    );
+  } else {
+    const blogs = response.content as BlogType[];
+    return (
+      <div>
+        <TypographyH2 className="mb-14">{decodedSlug}</TypographyH2>
+
+        {/* each item */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+          {/* each item  */}
+          {blogs.map((blog) => (
+            <DoclifyBlogCard blog={blog} key={blog.slug} />
+          ))}
+        </div>
+        {/* post horizontal */}
+      </div>
+    );
+  }
+}
