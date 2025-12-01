726686ca6f9b62165d3000a2d308dadd48f7539d
Seyam <seyam.hossain.118833@gmail.com>
2025-12-01 14:47:57 +0600

    feat (route - blog): minimal design of blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,187 @@
-import { TypographyH1 } from "@/components/ui/typography";
+import { DoclifyAuthorMeta, DoclifyImage } from "@/components/ui/image";
+import {
+  TypographyH2,
+  TypographyH3,
+  TypographyP,
+} from "@/components/ui/typography";
+import { Calendar1Icon } from "lucide-react";
 
+const text = `Working in cybersecurity means you’re constantly playing a game of
+            catch-up. Every day you learn something new, but your work is also
+            never finished.`;
 export default function Page() {
-  return <TypographyH1>Blog page</TypographyH1>;
+  return (
+    <div>
+      <TypographyH2 className="mb-14">Blog Page</TypographyH2>
+
+      {/* all blogs */}
+      <div className="space-y-5">
+        {/* top blog */}
+        <div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 gap-5 md:gap-10 hover:bg-accent transition-all duration-500 p-2 rounded-2xl">
+          {/* image side */}
+          <div>
+            <DoclifyImage
+              src="https://plus.unsplash.com/premium_photo-1763922901071-374d798c5062?q=80&w=730&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              height={400}
+              width={800}
+              alt="Image"
+            />
+          </div>
+          {/* content side  */}
+          <div className="flex flex-col justify-center px-5 md:px-0">
+            <div className="flex items-center justify-start mb-4 text-primary text-sm md:text-base">
+              <DoclifyAuthorMeta username={"ayan519600"} />
+            </div>
+            <div className="flex items-center justify-start mb-4 text-primary text-sm md:text-base">
+              <Calendar1Icon className="h-4 w-4 mr-2" />
+              Published at{" "}
+              {new Date("2025-11-30T12:56:29.572+00:00").toLocaleString(
+                "en-GB",
+                {
+                  day: "2-digit",
+                  month: "long",
+                  year: "numeric",
+                }
+              )}
+            </div>
+            <TypographyH3>Blog name</TypographyH3>
+            <TypographyP>{text.slice(0, 80) + "..."}</TypographyP>
+          </div>
+        </div>
+
+        {/* rest of the blog */}
+        <div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 gap-5">
+          {/* each item  */}
+          <div className="space-y-5 hover:bg-accent transition-all duration-500 p-2 rounded-2xl">
+            {/* image side */}
+            <div>
+              <DoclifyImage
+                src="https://plus.unsplash.com/premium_photo-1763922901071-374d798c5062?q=80&w=730&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                height={400}
+                width={1200}
+                alt="Image"
+              />
+            </div>
+            {/* content side  */}
+            <div className="px-5 pb-5">
+              <div className="flex items-center justify-start mb-4 text-primary text-sm md:text-base">
+                <DoclifyAuthorMeta username={"ayan519600"} />
+              </div>
+              <div className="flex items-center justify-start mb-4 text-primary text-sm md:text-base">
+                <Calendar1Icon className="h-4 w-4 mr-2" />
+                Published at{" "}
+                {new Date("2025-11-30T12:56:29.572+00:00").toLocaleString(
+                  "en-GB",
+                  {
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric",
+                  }
+                )}
+              </div>
+              <TypographyH3>Blog name</TypographyH3>
+              <TypographyP>{text.slice(0, 80) + "..."}</TypographyP>
+            </div>
+          </div>
+          {/* each item  */}
+          <div className="space-y-5 hover:bg-accent transition-all duration-500 p-2 rounded-2xl">
+            {/* image side */}
+            <div>
+              <DoclifyImage
+                src="https://plus.unsplash.com/premium_photo-1763922901071-374d798c5062?q=80&w=730&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                height={400}
+                width={1200}
+                alt="Image"
+              />
+            </div>
+            {/* content side  */}
+            <div className="px-5 pb-5">
+              <div className="flex items-center justify-start mb-4 text-primary text-sm md:text-base">
+                <DoclifyAuthorMeta username={"ayan519600"} />
+              </div>
+              <div className="flex items-center justify-start mb-4 text-primary text-sm md:text-base">
+                <Calendar1Icon className="h-4 w-4 mr-2" />
+                Published at{" "}
+                {new Date("2025-11-30T12:56:29.572+00:00").toLocaleString(
+                  "en-GB",
+                  {
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric",
+                  }
+                )}
+              </div>
+              <TypographyH3>Blog name</TypographyH3>
+              <TypographyP>{text.slice(0, 80) + "..."}</TypographyP>
+            </div>
+          </div>
+          {/* each item  */}
+          <div className="space-y-5 hover:bg-accent transition-all duration-500 p-2 rounded-2xl">
+            {/* image side */}
+            <div>
+              <DoclifyImage
+                src="https://plus.unsplash.com/premium_photo-1763922901071-374d798c5062?q=80&w=730&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                height={400}
+                width={1200}
+                alt="Image"
+              />
+            </div>
+            {/* content side  */}
+            <div className="px-5 pb-5">
+              <div className="flex items-center justify-start mb-4 text-primary text-sm md:text-base">
+                <DoclifyAuthorMeta username={"ayan519600"} />
+              </div>
+              <div className="flex items-center justify-start mb-4 text-primary text-sm md:text-base">
+                <Calendar1Icon className="h-4 w-4 mr-2" />
+                Published at{" "}
+                {new Date("2025-11-30T12:56:29.572+00:00").toLocaleString(
+                  "en-GB",
+                  {
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric",
+                  }
+                )}
+              </div>
+              <TypographyH3>Blog name</TypographyH3>
+              <TypographyP>{text.slice(0, 80) + "..."}</TypographyP>
+            </div>
+          </div>
+          {/* each item  */}
+          <div className="space-y-5 hover:bg-accent transition-all duration-500 p-2 rounded-2xl">
+            {/* image side */}
+            <div>
+              <DoclifyImage
+                src="https://plus.unsplash.com/premium_photo-1763922901071-374d798c5062?q=80&w=730&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                height={400}
+                width={1200}
+                alt="Image"
+              />
+            </div>
+            {/* content side  */}
+            <div className="px-5 pb-5">
+              <div className="flex items-center justify-start mb-4 text-primary text-sm md:text-base">
+                <DoclifyAuthorMeta username={"ayan519600"} />
+              </div>
+              <div className="flex items-center justify-start mb-4 text-primary text-sm md:text-base">
+                <Calendar1Icon className="h-4 w-4 mr-2" />
+                Published at{" "}
+                {new Date("2025-11-30T12:56:29.572+00:00").toLocaleString(
+                  "en-GB",
+                  {
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric",
+                  }
+                )}
+              </div>
+              <TypographyH3>Blog name</TypographyH3>
+              <TypographyP>{text.slice(0, 80) + "..."}</TypographyP>
+            </div>
+          </div>
+        </div>
+        {/* post horizontal */}
+      </div>
+    </div>
+  );
 }
--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -21,6 +21,44 @@ export function TypographyH1({
   );
 }
 
+export function TypographyH2({
+  children,
+  className,
+}: {
+  className?: string;
+  children: ReactNode;
+}) {
+  return (
+    <h2
+      className={cn(
+        "scroll-m-20 border-b pb-2 text-2xl md:text-3xl font-semibold tracking-tight first:mt-0",
+        className
+      )}
+    >
+      {children}
+    </h2>
+  );
+}
+
+export function TypographyH3({
+  children,
+  className,
+}: {
+  className?: string;
+  children: ReactNode;
+}) {
+  return (
+    <h3
+      className={cn(
+        "scroll-m-20 text-xl md:text-2xl font-semibold tracking-tight",
+        className
+      )}
+    >
+      {children}
+    </h3>
+  );
+}
+
 export function TypographyP({
   children,
   className,
