70c23d8d1b93fd8726b783d80d41af6f420ab3c4
Seyam <seyam.hossain.118833@gmail.com>
2025-12-01 20:48:49 +0600

    fix (feature): added metadata to page

--- src/app/categories/[slug]/page.tsx
+++ src/app/categories/[slug]/page.tsx
@@ -2,12 +2,24 @@ import { getSingleMeta } from "@/actions/post/post-actions";
 import { DoclifyBlogCard } from "@/components/DoclifyCards/DoclifyCards";
 import { TypographyH2 } from "@/components/ui/typography";
 import { BlogType } from "@/types/schema.types";
+import { Metadata } from "next";
 
-export default async function page({
-  params,
-}: {
+type Props = {
   params: Promise<{ slug: string }>;
-}) {
+};
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+
+  const decodedSlug = decodeURIComponent(slug);
+  const title = decodedSlug[0].toUpperCase() + decodedSlug.slice(1);
+
+  return {
+    title: title,
+    description: "Doclify Category",
+  };
+}
+
+export default async function page({ params }: Props) {
   const { slug } = await params;
   const decodedSlug = decodeURIComponent(slug);
   const response = await getSingleMeta("categories", decodedSlug);
@@ -15,14 +27,16 @@ export default async function page({
   if (response.success === false) {
     return (
       <div className="m-auto">
-        <TypographyH2 className="mb-14">{response.message}</TypographyH2>
+        <TypographyH2 className="mb-14 capitalize">
+          {response.message}
+        </TypographyH2>
       </div>
     );
   } else {
     const blogs = response.content as BlogType[];
     return (
       <div>
-        <TypographyH2 className="mb-14">{decodedSlug}</TypographyH2>
+        <TypographyH2 className="mb-14 capitalize">{decodedSlug}</TypographyH2>
 
         {/* each item */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
