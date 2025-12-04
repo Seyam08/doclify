2cac6913fd1de21aba4360db199623497aa66a0f
Seyam <seyam.hossain.118833@gmail.com>
2025-12-05 02:50:43 +0600

    feat (route - author): added author post section at single author

--- src/app/author/[slug]/page.tsx
+++ src/app/author/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { getAuthor } from "@/actions/author/author-action";
 import { getPostByAuthor } from "@/actions/post/post-actions";
+import { DoclifyBlogCard } from "@/components/DoclifyCards/DoclifyCards";
 import { DoclifySocialLinkShow } from "@/components/DoclifyItem/DoclifyItem";
 import { DoclifyImage } from "@/components/ui/image";
 import { Separator } from "@/components/ui/separator";
@@ -9,13 +10,33 @@ import {
   TypographyP,
 } from "@/components/ui/typography";
 import { AuthorType, BlogType } from "@/types/schema.types";
-import { SquarePen } from "lucide-react";
+import { Sparkles, SquarePen } from "lucide-react";
+import { Metadata } from "next";
 
-export default async function page({
-  params,
-}: {
+type Props = {
   params: Promise<{ slug: string }>;
-}) {
+};
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+
+  // fetch post information
+  const response = await getAuthor(slug);
+
+  if (response.success === true) {
+    const author = response.content as AuthorType;
+    return {
+      title: author.authorInfo.name,
+      description: "Doclify Author",
+    };
+  } else {
+    return {
+      title: "Author Not Found",
+      description: "Doclify",
+    };
+  }
+}
+
+export default async function page({ params }: Props) {
   const { slug } = await params;
   const response = await getAuthor(slug);
 
@@ -35,45 +56,76 @@ export default async function page({
     const socialLink = author.authorInfo.socialLinks; // social link can be an empty array
 
     return (
-      <div className="content-holder flex flex-col items-center space-y-5">
-        <div className="w-36 h-36">
-          <DoclifyImage
-            src={author.authorInfo.image}
-            alt={author.authorInfo.name}
-            height={200}
-            width={200}
-          />
-        </div>
-        <TypographyH3>{author.authorInfo.name}</TypographyH3>
-        <TypographyP className="mt-0 align-middle">
-          {blogResponse.success ? (
-            <>
-              <SquarePen className="inline-block h-4 w-4" /> {numberOfBlogs}{" "}
-              Published posts
-            </>
-          ) : (
-            blogResponse.message
-          )}
-        </TypographyP>
-        {author.authorInfo.bio && (
-          <TypographyP className="text-center">
-            {author.authorInfo.bio}
-          </TypographyP>
-        )}
-        {socialLink && socialLink.length > 0 && (
-          <div className="max-w-full flex gap-3">
-            {socialLink.map((item, index) => (
-              <DoclifySocialLinkShow
-                key={index}
-                address={item.address}
-                platform={item.platform}
-                className="p-2"
-              />
-            ))}
+      <>
+        <div className="content-holder flex flex-col items-center space-y-5">
+          <div className="w-36 h-36">
+            <DoclifyImage
+              src={author.authorInfo.image}
+              alt={author.authorInfo.name}
+              height={200}
+              width={200}
+            />
           </div>
-        )}
-        <Separator className="my-5" />
-      </div>
+          <TypographyH3>{author.authorInfo.name}</TypographyH3>
+          <TypographyP className="mt-0 align-middle">
+            {blogResponse.success ? (
+              <>
+                <SquarePen className="inline-block h-4 w-4" /> {numberOfBlogs}{" "}
+                Published posts
+              </>
+            ) : (
+              blogResponse.message
+            )}
+          </TypographyP>
+          {author.authorInfo.bio && (
+            <TypographyP className="text-center">
+              {author.authorInfo.bio}
+            </TypographyP>
+          )}
+          {socialLink && socialLink.length > 0 && (
+            <div className="max-w-full flex gap-3">
+              {socialLink.map((item, index) => (
+                <DoclifySocialLinkShow
+                  key={index}
+                  address={item.address}
+                  platform={item.platform}
+                  className="p-2"
+                />
+              ))}
+            </div>
+          )}
+          <Separator className="my-5" />
+        </div>
+        {/* post of author  */}
+        <div className="m-auto">
+          <TypographyH3 className="font-normal text-center">
+            Post of
+          </TypographyH3>
+
+          <TypographyH2 className="flex items-center justify-center gap-5 font-normal text-center mb-10">
+            <Sparkles className="h-5 w-5 fill-accent-foreground" />
+
+            {author.authorInfo.name}
+
+            <Sparkles className="h-5 w-5 fill-accent-foreground" />
+          </TypographyH2>
+          {/* each item */}
+          {blogs ? (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+              {/* each item  */}
+              {blogs.map((blog) => (
+                <DoclifyBlogCard blog={blog} key={blog.slug} />
+              ))}
+            </div>
+          ) : (
+            <div className="m-auto">
+              <TypographyH2 className="mb-14 capitalize">
+                This user don't have any Blog
+              </TypographyH2>
+            </div>
+          )}
+        </div>
+      </>
     );
   }
 }
