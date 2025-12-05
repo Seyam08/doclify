073c9d11e6906687e8d22177fce0e3402b34cb34
Seyam <seyam.hossain.118833@gmail.com>
2025-12-06 01:11:07 +0600

    feat (ui - component): added DoclifyAuthorCard

--- src/components/DoclifyAuthor/DoclifyAuthor.tsx
+++ src/components/DoclifyAuthor/DoclifyAuthor.tsx
@@ -1,5 +1,8 @@
 import { getAuthor } from "@/actions/author/author-action";
+import { getPostByAuthor } from "@/actions/post/post-actions";
+import { DoclifyBlogMiniCard } from "@/components/DoclifyCards/DoclifyCards";
 import { Button } from "@/components/ui/button";
+import { DoclifyImage } from "@/components/ui/image";
 import {
   Item,
   ItemActions,
@@ -12,8 +15,13 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import UnderlineLink from "@/components/ui/typography";
-import { ExternalLink, User } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import UnderlineLink, {
+  TypographyH3,
+  TypographyP,
+} from "@/components/ui/typography";
+import { AuthorType, BlogType } from "@/types/schema.types";
+import { ExternalLink, SquarePen, User } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React, { ReactNode } from "react";
@@ -108,5 +116,93 @@ export function DoclifyWarnPopover({
         </Item>
       </PopoverContent>
     </Popover>
+  );
+}
+
+export async function DoclifyAuthorCard({
+  author,
+  username,
+}: {
+  author: AuthorType;
+  username: string;
+}) {
+  const { authorInfo } = author;
+  const { email, image, name, bio, socialLinks } = authorInfo;
+  const blogResponse = await getPostByAuthor(username);
+  const blogs = blogResponse.content as BlogType[]; // blogs can be undefine or null
+  const numberOfBlogs = blogs?.length; // number of blog can be undefine also, because of blog
+  return (
+    <div className="bg-accent rounded-2xl p-3">
+      {/* author info */}
+      <div className="flex flex-row flex-wrap">
+        <div className="basis-full md:basis-1/4 text-start">
+          <DoclifyImage
+            src={image}
+            alt={name}
+            height={150}
+            width={150}
+            className="block w-fit"
+          />
+        </div>
+        <div className="basis-full md:basis-3/4 flex flex-col justify-center px-0 md:px-2 py-2 md:py-0">
+          <TypographyH3>
+            <UnderlineLink href={`/author/${username}`}>{name}</UnderlineLink>
+          </TypographyH3>
+
+          {bio && (
+            <TypographyP>
+              {bio.slice(0, 80)}...
+              <UnderlineLink
+                href={`/author/${username}`}
+                className="ml-1 text-accent-foreground"
+              >
+                Read More
+              </UnderlineLink>
+            </TypographyP>
+          )}
+        </div>
+      </div>
+
+      <Separator className="w-full my-3" />
+      {/* recent post */}
+      <div>
+        {/* title  */}
+        <div>
+          <TypographyP className="my-3 text-accent-foreground font-medium">
+            Recent Post
+          </TypographyP>
+        </div>
+        {/* posts  */}
+        {numberOfBlogs > 0 ? (
+          <div className="flex flex-row flex-wrap">
+            {/* each blog  */}
+            {blogs.slice(0, 2).map((blog) => (
+              <div className="basis-full md:basis-1/2" key={blog.slug}>
+                <DoclifyBlogMiniCard blog={blog} />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="py-5">
+            <TypographyH3 className="capitalize text-muted-foreground text-sm md:text-base">
+              This user doesn&apos;t have any blog
+            </TypographyH3>
+          </div>
+        )}
+        {numberOfBlogs > 2 && (
+          <div className="text-muted-foreground">
+            <SquarePen className="inline-block h-4 w-4" /> {name} has{" "}
+            <UnderlineLink
+              href={`/author/${username}`}
+              className="text-accent-foreground"
+            >
+              {numberOfBlogs - 2} more
+            </UnderlineLink>{" "}
+            Post to Read
+          </div>
+        )}
+        {/* each item */}
+      </div>
+    </div>
   );
 }
