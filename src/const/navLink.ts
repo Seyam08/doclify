849670e98eade1e488aef1444b7c207dbde9df9f
Seyam <seyam.hossain.118833@gmail.com>
2025-12-06 18:29:48 +0600

    feat(const): create navLink constants for navigation links

---
+++ src/const/navLink.ts
@@ -0,0 +1,8 @@
+import { NavbarNavLink } from "@/components/ui/navbar";
+
+export const navLink: NavbarNavLink[] = [
+  { href: "/", label: "Home" },
+  { href: "/blog", label: "Latest" },
+  { href: "/categories", label: "Categories" },
+  { href: "/tags", label: "Tags" },
+];
