15b16f1112d414767c5b1e756c94440927f13502
Seyam <seyam.hossain.118833@gmail.com>
2025-12-06 18:34:57 +0600

    feat(const): add 'Authors' link to navigation constants

--- src/const/navLink.ts
+++ src/const/navLink.ts
@@ -3,6 +3,7 @@ import { NavbarNavLink } from "@/components/ui/navbar";
 export const navLink: NavbarNavLink[] = [
   { href: "/", label: "Home" },
   { href: "/blog", label: "Latest" },
+  { href: "/author", label: "Authors" },
   { href: "/categories", label: "Categories" },
   { href: "/tags", label: "Tags" },
 ];
