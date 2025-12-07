e98bc88d997462fe01f91b696fa9eb916c356de4
Seyam <seyam.hossain.118833@gmail.com>
2025-12-07 22:13:38 +0600

    feat(route - home): add FeaturedPosts component and integrate tailwindcss-animated for enhanced animations

---
+++ src/components/DoclifySections/FeaturedPosts.tsx
@@ -0,0 +1,56 @@
+import { getLimitedPost } from "@/actions/post/post-actions";
+import { DoclifyBlogCard } from "@/components/DoclifyCards/DoclifyCards";
+import { TypographyH3 } from "@/components/ui/typography";
+import { cn } from "@/lib/utils";
+import { BlogType } from "@/types/schema.types";
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+import { ComponentProps } from "react";
+
+export default async function FeaturedPosts({
+  title,
+  limit,
+  className,
+}: {
+  limit: number;
+  title: string;
+} & ComponentProps<"div">) {
+  const response = await getLimitedPost(limit);
+
+  if (response.success === false) {
+    return (
+      <div className="m-auto text-center py-20">
+        <TypographyH3 className="mb-14 capitalize">
+          {response.message}
+        </TypographyH3>
+      </div>
+    );
+  } else {
+    const blogs = response.content as BlogType[];
+    return (
+      <div className={cn("w-full", className)}>
+        <div className="flex flex-row justify-between items-center mb-14">
+          <TypographyH3 className="capitalize">{title}</TypographyH3>
+          <Link
+            href="/blog"
+            className="relative group w-fit overflow-hidden flex items-center leading-7 gap-1"
+          >
+            All Blogs
+            <ArrowUpRight className="h-5 w-5 group-hover:rotate-45 transition-all duration-300" />
+            {/* bottom border using after */}
+            <span className="group-hover:-translate-x-full absolute left-0 bottom-0.5 h-0.5 w-full bg-current after:content-[''] transition-all duration-300"></span>
+          </Link>
+        </div>
+
+        {/* each item */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+          {/* each item  */}
+          {blogs.map((blog) => (
+            <DoclifyBlogCard blog={blog} key={blog.slug} />
+          ))}
+        </div>
+        {/* post horizontal */}
+      </div>
+    );
+  }
+}
