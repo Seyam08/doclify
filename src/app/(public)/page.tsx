ee1ace410f1c4053bbb636e80abf831587b38586
Seyam <seyam.hossain.118833@gmail.com>
2025-12-08 04:05:10 +0600

    fix (route - home): replace div elements with section tags for semantic structure

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -7,9 +7,9 @@ import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
-    <div className={cn("width-holder", "py-0 pt-5 md:pt-10")}>
+    <section className={cn("width-holder", "py-0 pt-5 md:pt-10")}>
       {/* hero section */}
-      <div className="content-holder m-auto space-y-10 py-28 px-5 text-center">
+      <section className="content-holder m-auto space-y-10 py-28 px-5 text-center">
         <TypographyH1 className="text-3xl md:text-5xl animate-fade-down animate-duration-700">
           Sharing ideas, stories, and
           <br /> lessons along the way
@@ -18,29 +18,29 @@ export default function Home() {
           Share your knowledge, tell your stories, or explore what others are
           writing. Everyone is welcome.
         </TypographyP>
-      </div>
+      </section>
 
       <Separator className="w-full h-1" />
 
       {/* latest post */}
 
-      <div className="py-10">
+      <section className="py-10">
         <FeaturedPosts title="Latest Blogs" limit={3} />
-      </div>
+      </section>
 
       <Separator className="w-full h-1" />
 
       {/* categories */}
-      <div className="py-10">
+      <section className="py-10">
         <TopCategories title="Top Categories" limit={6} />
-      </div>
+      </section>
 
       <Separator className="w-full h-1" />
 
       {/* newsletter */}
-      <div>
+      <section>
         <NewsLetter />
-      </div>
-    </div>
+      </section>
+    </section>
   );
 }
