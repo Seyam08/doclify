bf1b9c8600313e2e3f9753891386972c0bc2d1e3
Seyam <seyam.hossain.118833@gmail.com>
2025-12-07 20:34:14 +0600

    refactor(layout): replace div with main for semantic HTML structure

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -8,9 +8,9 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="relative w-full bg-[radial-gradient(circle_at_top_center,rgb(161,210,247,0.6),transparent_70%)] dark:bg-[radial-gradient(circle_at_top_center,rgb(2,13,25,1),transparent_70%)] bg-fixed">
+    <main className="relative w-full bg-[radial-gradient(circle_at_top_center,rgb(161,210,247,0.6),transparent_70%)] dark:bg-[radial-gradient(circle_at_top_center,rgb(2,13,25,1),transparent_70%)] bg-fixed">
       <Navbar userProfile={<UserProfile />} navigationLinks={navLink} />
       {children}
-    </div>
+    </main>
   );
 }
