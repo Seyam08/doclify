4fbb623fe5e7a90fca353c123dc9b62938fa2887
Seyam <seyam.hossain.118833@gmail.com>
2025-12-11 20:29:51 +0600

    feat (component): added breadcrumb

---
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+} from "@/components/ui/breadcrumb";
+import { TypographyP, UnderlineLink03 } from "@/components/ui/typography";
+import { House, Slash } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+export function DoclifyBreadcrumb() {
+  const pathname = usePathname();
+  const segments = pathname.split("/").filter(Boolean);
+  const crumbs = segments.map((seg, i) => {
+    const href = "/" + segments.slice(0, i + 1).join("/");
+    const label = seg.charAt(0).toUpperCase() + seg.slice(1);
+
+    return { href, label };
+  });
+  const lastItem = crumbs.length - 1;
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList className="gap-0.5 sm:gap-0.5">
+        <BreadcrumbItem>
+          <House className="h-4 w-4" />
+          <BreadcrumbLink asChild>
+            <UnderlineLink03 href="/" className="text-foreground">
+              Home
+            </UnderlineLink03>
+          </BreadcrumbLink>
+          <div className="[&>svg]:size-3.5">
+            <Slash className="-rotate-12" />
+          </div>
+        </BreadcrumbItem>
+        {crumbs.map((item, index) => (
+          <BreadcrumbItem key={index}>
+            <BreadcrumbLink asChild>
+              {lastItem !== index ? (
+                <UnderlineLink03 href={item.href} className="text-foreground">
+                  {item.label}
+                </UnderlineLink03>
+              ) : (
+                <TypographyP>{item.label}</TypographyP>
+              )}
+            </BreadcrumbLink>
+            {lastItem !== index && (
+              <div className="[&>svg]:size-3.5">
+                <Slash className="-rotate-12" />
+              </div>
+            )}
+          </BreadcrumbItem>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
