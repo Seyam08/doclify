a03e05c53aedf749bad16756106e3c52c4ebed33
Seyam <seyam.hossain.118833@gmail.com>
2025-12-14 19:29:29 +0600

    fix (public - layout): added suspense fallback for public route segment layout Navbar

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,7 +1,9 @@
 import { UserProfile } from "@/components/authentication/user-profile";
 import Footer from "@/components/DoclifySections/Footer";
+import { NavbarSkeleton } from "@/components/DoclifySkeleton/DoclifySkeleton";
 import { Navbar } from "@/components/ui/navbar";
 import { navLink } from "@/const/navLink";
+import { Suspense } from "react";
 
 export default function Layout({
   children,
@@ -10,7 +12,9 @@ export default function Layout({
 }>) {
   return (
     <main className="relative w-full bg-[radial-gradient(circle_at_top_center,rgb(161,210,247,0.6),transparent_70%)] dark:bg-[radial-gradient(circle_at_top_center,rgb(2,13,25,1),transparent_70%)] bg-fixed">
-      <Navbar userProfile={<UserProfile />} navigationLinks={navLink} />
+      <Suspense fallback={<NavbarSkeleton />}>
+        <Navbar userProfile={<UserProfile />} navigationLinks={navLink} />
+      </Suspense>
       {children}
       <Footer />
     </main>
