7e13092f91ffc8c5fd4012fd241fe5ea6bcffe46
Seyam <seyam.hossain.118833@gmail.com>
2025-12-14 21:45:37 +0600

    fix (public - layout): added Suspense boundary for UserProfile

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,6 +2,7 @@ import { UserProfile } from "@/components/authentication/user-profile";
 import Footer from "@/components/DoclifySections/Footer";
 import { NavbarSkeleton } from "@/components/DoclifySkeleton/DoclifySkeleton";
 import { Navbar } from "@/components/ui/navbar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { navLink } from "@/const/navLink";
 import { Suspense } from "react";
 
@@ -13,7 +14,14 @@ export default function Layout({
   return (
     <main className="relative w-full bg-[radial-gradient(circle_at_top_center,rgb(161,210,247,0.6),transparent_70%)] dark:bg-[radial-gradient(circle_at_top_center,rgb(2,13,25,1),transparent_70%)] bg-fixed">
       <Suspense fallback={<NavbarSkeleton />}>
-        <Navbar userProfile={<UserProfile />} navigationLinks={navLink} />
+        <Navbar
+          userProfile={
+            <Suspense fallback={<Skeleton className="h-9 w-11.5" />}>
+              <UserProfile />
+            </Suspense>
+          }
+          navigationLinks={navLink}
+        />
       </Suspense>
       {children}
       <Footer />
