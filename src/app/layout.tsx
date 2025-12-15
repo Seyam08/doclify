5ae675c0d39693ec70463a5f3c0bcc0e3e02c8f5
Seyam <seyam.hossain.118833@gmail.com>
2025-12-15 19:22:19 +0600

    feat (favicon): added static favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body
         suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
